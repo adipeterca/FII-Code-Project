af65ffa091fc8ea08e3f6442ae1bb1d0e23c36e4
adipeterca <adipeterca@gmail.com>
2020-02-18 22:57:06 +0200

    Minor changes.
    There are still some bugs left to resolve.

--- Harap Alb - The Awakening/Assets/Scripts/Movement.cs
+++ Harap Alb - The Awakening/Assets/Scripts/Movement.cs
@@ -30,40 +30,80 @@ public class Movement : MonoBehaviour
     ///  Reference to the Sprite Renderer component.
     SpriteRenderer playerSprite;
 
+
+    bool canJump;
+
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
         currentTime = 0;
         isDucking = false;
+        canJump = true;
     }
+
+    //private void FixedUpdate()
+    //{
+    //    moveV = Input.GetAxisRaw("Vertical");
+
+    //    currentTime -= Time.deltaTime;
+
+    //    if (moveV == -1 && currentTime <= 0)
+    //    {
+    //        playerSprite.sprite = duckSprite;
+    //        isDucking = true;
+    //        Debug.Log("Duck");
+    //    }
+    //    else
+    //    {
+    //        isDucking = false;
+    //        if(moveV == 0 && currentTime <=0)
+    //            playerSprite.sprite = defaultSprite;
+    //        if (currentTime <= 0 && moveV == 1)
+    //        {
+    //            playerRb.AddForce(new Vector2(0, moveV * jumpPower));
+    //            playerSprite.sprite = jumpSprite;
+    //            currentTime = jumpTime;
+    //            Debug.Log("Jump");
+    //        }   
+    //    }
+    //}
 
     private void FixedUpdate()
     {
         moveV = Input.GetAxisRaw("Vertical");
 
-        currentTime -= Time.deltaTime;
-
-        if (moveV == -1 && currentTime <= 0)
-        {
-            playerSprite.sprite = duckSprite;
-            isDucking = true;
-            Debug.Log("Duck");
-        }
-        else
+        if (moveV != -1)
         {
             isDucking = false;
-            if(moveV == 0 && currentTime <=0)
+            if (!canJump)
                 playerSprite.sprite = defaultSprite;
-            if (currentTime <= 0 && moveV == 1)
+            Debug.Log("He is not ducking.");
+        }
+
+        if (canJump)
+        {
+            if (moveV == -1)
             {
-                playerRb.AddForce(new Vector2(0, moveV * jumpPower));
-                playerSprite.sprite = jumpSprite;
-                currentTime = jumpTime;
-                Debug.Log("Jump");
-            }   
+                playerSprite.sprite = duckSprite;
+                isDucking = true;
+                canJump = false;
+                Debug.Log("Duck");
+            }
+            else
+            {
+                playerSprite.sprite = defaultSprite;
+                if (moveV == 1)
+                {
+                    playerRb.AddForce(new Vector2(0, moveV * jumpPower));
+                    playerSprite.sprite = jumpSprite;
+                    canJump = false;
+                    Debug.Log("Jump");
+                }
+            }
         }
     }
+
     private void Update()
     {
         moveH = Input.GetAxisRaw("Horizontal");
@@ -71,6 +111,15 @@ public class Movement : MonoBehaviour
         {
             transform.Translate(new Vector3(moveH * speed * Time.deltaTime, 0, 0));
             Debug.Log("Moved along the horizontal axis.");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Platform"))
+        {
+            canJump = true;
+            Debug.Log("Collision detected.");
         }
     }
 
