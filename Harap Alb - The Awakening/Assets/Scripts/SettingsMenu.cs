46b0a38bb58317ac479b582afa96117076df910f
Theodor Vlad <vladtheodor0@gmail.com>
2020-03-01 15:43:31 +0200

    Updated options section
    Added:
    -volume slider functionality
    -graphics quality dropdown
    -fullscreen/windowed toggle

---
+++ Harap Alb - The Awakening/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void Start()
+    {
+        audioMixer.SetFloat("volume", (float)-40);
+    }
+
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat("volume", value);
+    }
+
+    public void SetQuality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
+    }
+
+    public void SetFullscreen(bool isToggled)
+    {
+        Screen.fullScreen = isToggled;
+    }
+}
